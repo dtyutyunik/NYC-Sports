744f868923e5b062e82e665a41b9ea8fde1aed0a
Dima <dmitriy.tyutyunik@gmail.com>
2019-01-08 15:08:33 -0500

    ruby fun

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
 
 has_many :Bocce, through: :Favorite
 has_many :Basketball, through: :Favorite
+has_many :Tenni, through: :Favorite
+has_many :Cricket, through: :Favorite
+has_many :Pool, through: :Favorite
+has_many :Bocce, through: :Favorite
 has_secure_password
 
 validates :email, presence: true
