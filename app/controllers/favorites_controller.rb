caeb9172138a347380c6264737023e3280aa6793
Dima <dmitriy.tyutyunik@gmail.com>
2019-01-08 18:50:04 -0500

    delete favorites works

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,34 +1,13 @@
 class FavoritesController < ApplicationController
   before_action :set_favorite, only: [:show, :update, :destroy]
 
-# http://localhost:3000/favorites?user_id=1
+# http://localhost:3000/favorites?user_id=2
   # GET /favorites
   def index
+#shows all info of speciifc user
+# @user= User.find(params[:user_id]).to_json(include: [:pools, :basketballs, :bocces, :crickets, :handballs, :tennis] )
 
-    # p params[:user_id]
-    #
-    # @favorites= Favorite.joins
-    # Favorite.all(params[:user_id])
-    # @favorites = Favorite.find(params[:user_id])
-    # @favorites = Favorite.joins(:favorites).where(:favorites => {:user_id => params[:user:id]}
-
-# this queries based on the user_id we entered
-# @favorites = Favorite.where(:favorites => {:user_id => params[:user_id]})
-
-# this gets me all the pools name and location
-# @favorites = Favorite.where(:favorites => {:user_id => params[:user_id]}).includes(:pool).pluck(:name, :location)
-
-
-# @favorites = Favorite.where(:favorites => {:user_id => params[:user_id]}).includes(:pool).pluck(:name, :location)
-# @favorites = Favorite.where(:favorites => {:user_id => params[:user_id]}).includes(:pool)
 @user= User.find(params[:user_id]).to_json(include: [:pools, :basketballs, :bocces, :crickets, :handballs, :tennis] )
-
-
-
-# Three relevant tables: users, posts, and comments
-
-# @comments = Comment.joins(:post).where(:posts => { :user_id => @user.id })
-
 
 
     render json: @user
@@ -36,18 +15,11 @@ class FavoritesController < ApplicationController
 
 
 # http://localhost:3000/favorites/?user_id=4&sport=basketball&sportid=9
-  # post /favoriteCustom/userid/sportscolumn/sportsid
   def create
-
-    # p params
-    # p params[:user_id]
-    # p params[:sport]
-
-    # "Hello, #{name}!"
 
     #depending on sport we send it will become sport_id so we can target the right cell
     sportKind="#{params[:sport]}_id"
-    p sportKind
+    # p sportKind
 
     #user_id is the userid we send through
     #sportkind is the type of sport and the sportid is the id of the sport that will register on the table
@@ -58,44 +30,20 @@ class FavoritesController < ApplicationController
       render json: @favorite.errors, status: :unprocessable_entity
      end
 
-    # p params[:userid]
-    # params[:controller] params[:action]
-
-# param: :slug
-    # render json: {userId => userId, sportsColumn => sportsColumn, sportsID => sportsID}
-
-    # @favorite = Favorite.new(favorite_custom_params)
-    #
-    #   if @favorite.save
-    #     render json: @favorite, status: :created, location: @favorite
-    #   else
-    #     render json: @favorite.errors, status: :unprocessable_entity
-    #   end
-
   end
 
-  # POST /favorites
-  # def create
-  #   @favorite = Favorite.new(favorite_params)
-  #
-  #   if @favorite.save
-  #     render json: @favorite, status: :created, location: @favorite
-  #   else
-  #     render json: @favorite.errors, status: :unprocessable_entity
-  #   end
-  # end
 
-  # PATCH/PUT /favorites/1
-  def update
-    if @favorite.update(favorite_params)
-      render json: @favorite
-    else
-      render json: @favorite.errors, status: :unprocessable_entity
-    end
-  end
 
   # DELETE /favorites/1
   def destroy
+# p params
+
+# @favorite=params[:user_id]
+# http://localhost:3000/favorites/?user_id=2&sport=basketball&sportid=11
+
+# sportKind="#{params[:sport]}_id"
+
+# render json: params[:user_
     @favorite.destroy
   end
 
