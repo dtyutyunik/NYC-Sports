0cf62bf7221a518a8a20ebc69e098d9bcdac80ef
Dima <dmitriy.tyutyunik@gmail.com>
2019-01-10 17:39:18 -0500

    deployment protected

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -4,11 +4,9 @@ class MapController < ApplicationController
   def show
     first = params[:first]
     second = params[:second]
-    third= params[:third]
+    # third= params[:third]
     # apiKey = ENV.fetch('REACT_APP_GOOGLE_CLIENT_ID')
-    # resp = RestClient.get('https://maps.googleapis.com/maps/api/distancematrix/json?origins=Vancouver+BC|Seattle&destinations=San+Francisco|Vancouver+BC&mode=bicycling&language=fr-FR&key=AIzaSyCUR73C8B9YkSXexkRRodfVzMuLLB20Ry8')
-    resp = RestClient.get("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=#{first}+NY&destinations=#{second}+NY&mode=walking&language=en-EN&key=AIzaSyCUR73C8B9YkSXexkRRodfVzMuLLB20Ry8");
-    # resp = RestClient.get("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=#{first}+NY&destinations=#{second}+NY&mode=walking&language=en-EN&key=#{third}");
+    resp = RestClient.get("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=#{first}+NY&destinations=#{second}+NY&mode=walking&language=en-EN&key=#{Rails.application.credentials.google_api_key}");
     render json: resp
   end
 end
